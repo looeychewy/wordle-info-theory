1054200ba0e212fdf99760a2cd4a34787dafcf5d
looeychewy <143098413+looeychewy@users.noreply.github.com>
2026-04-17 13:12:14 -0400

    keyboard file

--- keyboard_test.py
+++ keyboard_test.py
@@ -1,26 +1,28 @@
 """
 Super basic keyboard GUI app with tkinter -> utilize for actual Wordle keyboard hopefully
-Enter/Del functionality needs to be implemented (shows up as words currently)
+- Enter/Del functionality needs to be implemented (shows up as words currently)
     - Del functionality done 4/15
     - Enter WIP
-Limit text entered FROM KEYBOARD to 5 characters
-Link IRL keyboard to actually type on the screen without needing to click into the Entry box
+- Limit text entered FROM KEYBOARD to 5 characters
+- Link IRL keyboard to actually type on the screen without needing to click into the Entry box
     - display.focus_set() -> done 4/15
+- Capitalized input into Entry widget
+    - Done 4/17
 """
 
 import tkinter as tk
 from tkinter import *
 
-# Instantiate new instance of the tk class
 root = tk.Tk()
-
 root.title("Wordle Keyboard")
 root.configure(background="Dark grey")
 root.geometry("1260x500+150+200") # +[VALUE] sets on-screen pos.
 
+# Sets main title for the application window
 main_title = Label(root, width=39, bg='Dark Grey', font=('arial', 40, 'bold'), text='\tWordle Keyboard\t', padx=12)
 main_title.grid(row=0, column=0)
 
+# Container to hold + organize all keyboard buttons
 main_frame = Frame(root, bg ='Dark Grey', bd = 10, width = 1250, height = 490, relief="flat")
 main_frame.grid(row = 1, column = 0, padx = 100, pady = 30)
 
@@ -30,11 +32,17 @@ keys = [ ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
          ['ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'DEL', 'CLEAR'] # How implement ENTER & DEL functionality?
 ]
 
-# Display outputted text (Entry object)
-display = tk.Entry(main_frame, font = ('arial', 28), bd = 5, width = 54)
-display_text = StringVar()
 
+# Converts keyboard input into uppercase letters
+def uppercase_conversion(*args):
+    display_text.set(display.get().upper())
+
+# Display outputted text (Entry object)
+display_text = StringVar()
+display = tk.Entry(root, textvariable = display_text, font=('arial', 28), bd = 5, width = 54)
 display.focus_set() # Able to type into Entry widget without first clicking on it
+
+display_text.trace('w', uppercase_conversion)
 display.grid(row = 0, column = 0, columnspan = len(keys[0]), pady = 10)
 
 for i, key_row in enumerate(keys):
@@ -43,22 +51,22 @@ for i, key_row in enumerate(keys):
                   button_click(key)).grid(row = i+2, column = j))
 
 # Main button click functionality
-def button_click(key):
-    if key == 'DEL':
+def button_click(kb_key):
+    if kb_key == 'DEL':
         print("del active")
         display.delete(len(display.get()) - 1, 'end')
-    elif key == 'ENTER':
+    elif kb_key == 'ENTER':
         display.delete(0, tk.END)
         display.insert(0, str("String passed"))
         pass
         # return entry into wordle
-    elif key == 'CLEAR':
+    elif kb_key == 'CLEAR':
         display.delete(0, tk.END)
     else:
         print("normal active")
         current = display.get()
         display.delete(0, tk.END)
-        display.insert(0, str(current) + str(key))
+        display.insert(0, str(current) + str(kb_key))
 
         # Basic character entry limit (irl keyboard input not limited yet)
         if len(display.get()) > 5:
